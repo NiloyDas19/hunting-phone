3ac90c53e57b407c991a19fbb2ea38139f401cde
Niloy Das <niloydas5215@gmail.com>
2024-03-01 12:17:38 +0600

    show phone done

--- js/phone.js
+++ js/phone.js
@@ -1,8 +1,19 @@
 
 
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText='13', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
+    console.log(data);
+    if(data.status === false){
+        const phoneContainer = document.getElementById('phone-container');
+        phoneContainer.textContent = '';
+        const showMassage = document.createElement('p');
+        showMassage.classList.add('text-center');
+        showMassage.innerText = 'No Phone Available in this name';
+        phoneContainer.appendChild(showMassage);
+        toggleLoadingSpinner(false);
+        return;
+    }
     const phones = data.data;
     // console.log(phones);
     displayPhones(phones, isShowAll);
@@ -11,7 +22,7 @@ const loadPhone = async (searchText, isShowAll) => {
 
 
 const displayPhones =(phones, isShowAll) => {
-    console.log(phones);
+    // console.log(phones);
 
     // 1. get the element
     const phoneContainer = document.getElementById('phone-container');
@@ -33,7 +44,7 @@ const displayPhones =(phones, isShowAll) => {
 
 
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // 2. Create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card p-4 bg-base-100 shadow-xl`;
@@ -44,8 +55,9 @@ const displayPhones =(phones, isShowAll) => {
             <div class="card-body text-center">
                 <h2 class="">${phone.phone_name}</h2>
                 <p>There are many variations of passages of available, but the majority have suffered</p>
+                <p>$999</p>
                 <div class="card-actions justify-center">
-                    <button class="btn btn-primary">Show Details</button>
+                    <button class="btn btn-primary" onClick = "show_details_modal.showModal(); handleShowDetail('${phone.slug}')">Show Details</button>
                 </div>
             </div>
         `;
@@ -54,6 +66,47 @@ const displayPhones =(phones, isShowAll) => {
         phoneContainer.appendChild(phoneCard);
     });
     toggleLoadingSpinner(false);
+}
+
+
+// handle show details 
+const handleShowDetail = async(id) =>{
+    // console.log(id);
+    // load single phone data 
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const singleMobile = data.data;
+    // console.log(singleMobile);
+    showPhoneDetails(singleMobile);
+} 
+
+
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const phoneImage = document.getElementById('phone-image');
+    const phoneName = document.getElementById('phone-name');
+    const phoneStorage = document.getElementById('phone-storage');
+    const phoneDisplaySize = document.getElementById('phone-display-size');
+    const phoneChipset = document.getElementById('phone-chipset');
+    const phoneMemory = document.getElementById('phone-memory');
+    const phoneSlug = document.getElementById('phone-slug');
+    const phoneReleaseDate = document.getElementById('phone-release-date');
+    const phoneBrand = document.getElementById('phone-brand');
+    const phoneGps = document.getElementById('phone-gps');
+
+    phoneImage.innerHTML = `
+        <img src="${phone.image}">
+    `;
+    phoneName.innerText = phone.name;
+    phoneStorage.innerText = phone.mainFeatures.storage;
+    phoneDisplaySize.innerText = phone.mainFeatures.displaySize;
+    phoneChipset.innerText = phone.mainFeatures.chipSet;
+    phoneMemory.innerText = phone.mainFeatures.memory;
+    phoneSlug.innerText = phone.slug;
+    phoneReleaseDate.innerText = phone.releaseDate;
+    phoneBrand.innerText = phone.brand;
+    phoneGps.innerText = phone.others?.GPS || "No GPS";
+
 }
 
 // handel Search
@@ -78,4 +131,4 @@ const handleShowAll = () =>{
     handelSearch(true);
 }
 
-// loadPhone();
+loadPhone();
