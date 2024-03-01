b36c3034678d66d5fecef5e4a4ef958cd2517663
Niloy Das <niloydas5215@gmail.com>
2024-03-01 10:16:56 +0600

    search option done and loading spinner added

--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,7 @@
 
 
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -15,6 +15,21 @@ const displayPhones = phones => {
 
     // 1. get the element
     const phoneContainer = document.getElementById('phone-container');
+
+    // clear the phoneContainer before adding new child
+    phoneContainer.textContent = '';
+
+    // display show all button if there are more than 12 phones
+    const showAllContainer = document.getElementById('show-all-container');
+    if(phones.length > 12){
+        showAllContainer.classList.remove('hidden');
+    }
+    else{
+        showAllContainer.classList.add('hidden');
+    }
+
+    // Display only first 12 phones
+    phones = phones.slice(0, 12);
 
     phones.forEach(phone => {
         console.log(phone);
@@ -37,7 +52,25 @@ const displayPhones = phones => {
         // 4. append the child
         phoneContainer.appendChild(phoneCard);
     });
+    toggleLoadingSpinner(false);
+    
+}
+
+// handel Search
+const handelSearch = () => {
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
 }
 
 
-loadPhone();
+// loading spinner 
+const toggleLoadingSpinner = (isLoading) =>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading) loadingSpinner.classList.remove('hidden');
+    else loadingSpinner.classList.add('hidden');
+}
+
+// loadPhone();
